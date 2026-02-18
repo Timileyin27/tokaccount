9680d5538e5e7d7b49857a3afcb7c991c0e93a6e
Timileyin27 <timileyinabdulazeez27@gmail.com>
2026-02-18 16:57:34 +0100

    add account and ui changes

--- app/routers/account.py
+++ app/routers/account.py
@@ -28,11 +28,16 @@ def read_accounts(request: Request, db:Session=Depends(get_db),current_user:app.
     ).scalar() or 0
 
     return templates.TemplateResponse("dashboard.html", {"request": request,"current_user":current_user, "accounts": accounts,"total_stock_value": total_stock_value,"total_sold_value": total_sold_value,"total_accounts": total_accounts,"total_revenue": total_revenue})
-@router.post("/", status_code=status.HTTP_201_CREATED,response_model=app.schema.AccountOut)
+@router.post("/", status_code=status.HTTP_201_CREATED,)
 def create_account( request: Request, name: str = Form(...), price: float = Form(...),amount_in_stock: int =Form(...),amount_sold: int =Form(...),db:Session=Depends(get_db),current_user:app.models.User=Depends(get_current_user)): 
     templates = Jinja2Templates(directory="app/templates/admin")
     account = app.models.Account(owner_id=current_user.id, name=name,price=price,amount_in_stock=amount_in_stock,amount_sold=amount_sold)
     db.add(account)
     db.commit()
-    db.refresh(account) 
-    return account
+    response = RedirectResponse(url="/Dashboard/", status_code=303)
+
+    return response
+@router.get("/add")
+def add_account(request: Request, db:Session=Depends(get_db),current_user:app.models.User=Depends(get_current_user)):
+    templates = Jinja2Templates(directory="app/templates/admin")
+    return templates.TemplateResponse("add_account.html", {"request": request,"current_user":current_user})
