042ca00ec9391cb9abc86058268b976907c29bc8
Timileyin27 <timileyinabdulazeez27@gmail.com>
2026-03-04 16:44:39 +0100

    done with cart whwn it is empty but not when fully

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 import app.models as models
-from app.routers import  account, user, auth
+from app.routers import  account, user, auth, cart
 import psycopg2
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -21,6 +21,8 @@ models.Base.metadata.create_all(bind=engine)
 app.router.include_router(account.router)
 app.router.include_router(user.router)
 app.router.include_router(auth.router)
+app.router.include_router(cart.router)
+
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 Jinja2Templates(directory="app/templates")
--- app/routers/cart.py
+++ app/routers/cart.py
@@ -15,7 +15,7 @@ router = APIRouter(
 @router.post("/add")
 def add_to_cart(account_id: int = Form(...),quantity: int = Form(...),request: Request = None, response: Response = None, db: Session = Depends(get_db)):
     cart_reference = request.cookies.get("cart_reference")
-    cart = get_or_create_cart(request,response, db,cart_reference)
+    cart = get_or_create_cart(request,response,db)
     account=db.query(app.models.Account).filter(app.models.Account.id == account_id).first()
     if not account:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Account with id: {account_id} not found")
@@ -31,3 +31,11 @@ def add_to_cart(account_id: int = Form(...),quantity: int = Form(...),request: R
     db.commit()
 
     return RedirectResponse(url="/cart/view", status_code=303)
+@router.get("/view")
+def view_cart(request: Request,response:Response, db: Session = Depends(get_db)):
+    cart_reference = request.cookies.get("cart_reference")
+    cart = get_or_create_cart(request, response, db)
+    cart_items = db.query(app.models.CartItem).filter(app.models.CartItem.cart_id == cart.id).all()
+    total = sum(item.quantity * item.account.price for item in cart_items)
+    templates = Jinja2Templates(directory="app/templates/public")
+    return templates.TemplateResponse("cart.html", {"request": request, "cart_items": cart_items, "total": total})
