4fbd6591fcca7911c7252bea6cef34a850f97284
Timileyin27 <timileyinabdulazeez27@gmail.com>
2026-04-08 09:40:30 +0100

    db commention added

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.config import settings
 DATABASE_URL = settings.database_url
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
